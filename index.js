22bc9ece2b62acf64df26713761c55129c91afa2
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-03 23:03:04 -0300

    refactor: organizing code

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const databaseConnection = require('./src/database/database');
 
 require('dotenv').config();
 
-databaseConnection();
 const PORT = process.env.PORT || 3001;
-
+databaseConnection();
 
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
   });
